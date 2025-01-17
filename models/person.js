6395f1998f4183724a0769e2c377b92e96fdf459
Praise W <praisewuche69@gmail.com>
2025-01-17 14:19:53 +0100

    comitme

--- models/person.js
+++ models/person.js
@@ -21,7 +21,7 @@ const personSchema = new mongoose.Schema({
         minLength: 8,
         validate:{
             validator: function(v){
-                return /\d{3}-\d{8}/.test(v) || /\d{2}-\d{8}/.test(v)
+                return /\d{3}-\d{8}/.test(v) || /\d{2}-\d{7}/.test(v)
             },
             message: props => `${props.value} is not a valid phone number!`
         }
